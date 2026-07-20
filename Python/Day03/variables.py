48024f36749eaef2f7b82867cfd14f4f15a0bd24
Dnyaneshwari Thombal <thombaldnyaneshwari40@gmail.com>
2026-07-20 20:18:52 +0530

    Completed Day 3 - variables and Data types

---
+++ Python/Day03/variables.py
@@ -0,0 +1,25 @@
+# Variable is like a container that holds data
+# creating a placeholder in memory
+
+c = complex(8,2)
+a= 12345
+d = True
+print(a)
+b="Dnyaneshwari" #strung data type that's why ""
+print(b)
+
+print("The type of a is ", type(a))
+print("The type of b is ", type(b))
+print("The type of c is ", type(c))
+print("The type of d is ", type(d))
+
+list1 = [8,2,3,[4,5],["apple","mango"]]
+print(list1) # collection of items
+
+tuple1 = (8,2,3,(4,5),("apple","mango"))
+print(tuple1) # collection of items but items cannot be changed
+
+# Dictionary collection of key value pairs
+
+dict1 ={"name":"Dnyaneshwari" ,"age":19}
+print(dict1) 
