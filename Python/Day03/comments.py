44d5f698af24486cc56b554f0cf3f31b9e5bc2b1
Dnyaneshwari Thombal <thombaldnyaneshwari40@gmail.com>
2026-07-20 19:38:18 +0530

    Completed Day 03 - Comments and Escape sequence character

---
+++ Python/Day03/comments.py
@@ -0,0 +1,12 @@
+''' Hey, Dont remove this line
+Author:- Dnyaneshwari
+'''
+print("Hello i am a girl\nthey are also good ")
+#This is single line comment
+print("hello python")
+
+#Escape sequence character
+
+print('Hey I am\' a \"good boy\"\nand this viewer is also a good boy /girl')
+print("Hey",6,7, sep="#", end="009\n")
+print("Dnyaneshwari")
