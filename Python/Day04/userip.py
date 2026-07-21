a105c781249805889672ee5371ffdc81f3fd22f5
Dnyaneshwari Thombal <thombaldnyaneshwari40@gmail.com>
2026-07-21 16:37:07 +0530

    Completed Day 04 - User Input

---
+++ Python/Day04/userip.py
@@ -0,0 +1,14 @@
+a = input("Enter your name")
+print("My name is:",a)
+
+
+# by default python takes string in input fun
+# so for no we have give int
+x = int(input("Enter 1st no"))
+y = int(input("Enter 2nd no"))
+print(x+y)
+print(x-y)
+print(x*y)
+print(x/y)
+
+
