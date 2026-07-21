b447302cac9f62edd1659d7cbc40d04bf925fac2
Dnyaneshwari Thombal <thombaldnyaneshwari40@gmail.com>
2026-07-21 16:03:09 +0530

    Completed Day 04 - Calculator

---
+++ Python/Day04/Calculator.py
@@ -0,0 +1,12 @@
+#int n1 =int(input("Enter 1st number"))
+#int n2= int(input("Enter 2nd number"))
+a = 10
+b = 5
+
+print("Addition is",(a+b))
+print("Subtraction is ",(a-b))
+print("Multiplication is",(a*b))
+print("Division is ",(a/b))
+print("Floor division is ",(a//b))
+
+
