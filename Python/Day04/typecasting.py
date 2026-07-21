0e918adfa228f5128c01bb0d16feddc14e6b899e
Dnyaneshwari Thombal <thombaldnyaneshwari40@gmail.com>
2026-07-21 16:22:22 +0530

    Completed Day 04 - Typecasting

---
+++ Python/Day04/typecasting.py
@@ -0,0 +1,19 @@
+#typecasting means  the conversion of 1 data type into another 
+# is called type casting
+
+
+a ='1'
+b='29'
+print(int(a) + int(b))
+
+# types 
+#implicit it will be done by python interpreter
+#explicit  the conversion will be done by programmer
+# implicit
+
+c = 1.9
+d = 8
+res = c + d
+print("The type of res is ",type(res))
+
+
