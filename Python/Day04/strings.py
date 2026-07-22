187d7bfac2c180144a5379941ed384e20b39651e
Dnyaneshwari Thombal <thombaldnyaneshwari40@gmail.com>
2026-07-23 01:21:46 +0530

    Completed string basics

---
+++ Python/Day04/strings.py
@@ -0,0 +1,24 @@
+# seq of character
+
+name = "Dnyaneshwari"
+print("hello,"+ name)
+
+#multiline string
+
+s1 = '''Hey Dnyanu,
+How are you??
+Are  you fine ...
+what about your python journey'''
+print(s1)
+ 
+# accessed using index
+
+print(name[0])
+print(name[11])
+
+#loop through the string s1 and name for printing all character
+for character in name:
+  print(character)
+
+for i in s1:
+  print(i)
