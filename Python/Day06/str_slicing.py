4dfae10d33412639f065d9b8988cf27f9141d89c
Dnyaneshwari Thombal <thombaldnyaneshwari40@gmail.com>
2026-07-24 02:51:31 +0530

    String slicing

---
+++ Python/Day06/str_slicing.py
@@ -0,0 +1,14 @@
+name ="Dnyaneshwari"
+print(name[0:5])
+
+print(len(name))
+
+fruit ="apple"
+alen=len(fruit)
+print(alen)
+print(fruit[0:4]) # including 0 but not 4
+print(fruit[1:4]) # including 1 but not 4
+print(fruit[:5])
+print(fruit[0:-3])
+#print(fruit[0:len(fruit)-3]) so i
+print(fruit[-3:-1])
