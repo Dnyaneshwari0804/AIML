e8a9561b4933963ba487cdce69b07c4e41250f0e
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 <Dnyaneshwari0804>
2026-07-28 11:27:24 +0530

    Completed - IF statements

---
+++ Python/Day08/if_statement.py
@@ -0,0 +1,6 @@
+#making decision in programming lang thats why control stmt are used 
+#checking even or odd no , eligible fro vote or not 
+
+a = int(input("Enter your age"))
+if a>=18:
+    print("Eligible for driving")    #executes only once when the condition is true
