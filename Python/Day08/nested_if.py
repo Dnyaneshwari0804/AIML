9998fca206056dc9222c6f6a11673708abaf17a3
Dnyaneshwari Thombal <thombaldnyaneshwari40@gmail.com>
2026-07-29 02:08:29 +0530

    Nested if Statements

---
+++ Python/Day08/nested_if.py
@@ -0,0 +1,12 @@
+n = 18
+if(n <0):
+  print("no is negative")
+elif(n>0):
+  if(n<=10):
+     print("no is between 1-10")
+  elif (n > 10 and n <=20):
+     print("no is between 11-20")
+  else:
+     print("no is greater than 20")
+else:
+  print("no is zero ")
