d8b9afe1938060034a2ecbe03fc3049a5f79ec1f
Dnyaneshwari Thombal <thombaldnyaneshwari40@gmail.com>
2026-07-30 19:31:55 +0530

    completed Operators

---
+++ Python/Day10/operators.py
@@ -0,0 +1,45 @@
+#Arithmetic operators
+
+a = 7
+b = 4
+c = a + b
+print(c)
+
+
+#Assignment operator
+
+a =4-2 # assign 4-2 in a
+print(a)
+b=6
+b+=3 #inc the value of b by 3 and then assign it to b
+b-=3  # dec the value of b by 3 and then assign it to b
+print(b)
+# comaprison opertaor
+
+d = 5<4 
+print(d)   
+
+# return always boolean value
+d = 5!=7
+print(d)
+d= 5==5
+print(d)
+
+#logical operator
+
+e = True or False
+#truth of or
+print("True or false is ", True or False)
+print("True or true is ", True or True)
+print("false or false is ", False or False)
+print("false or true is ", False or True)
+#truth table of and
+print("True or false is ", True and False)
+print("True or true is ", True and True)
+print("false or false is ", False and False)
+print("false or true is ", False and True)
+
+# truth table of and
+
+print(not(False))
+print(not(True)) # true to false and false to true
