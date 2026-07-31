94ec388eaaaab6decf81e9ee082e1fcaef511dd7
Gaurav <thombaldnyaneshwari40@gmail.com>
2026-07-31 21:55:05 +0530

    Completed Practice set

---
+++ Python/Day11/practice.py
@@ -0,0 +1,12 @@
+a = 10
+b = 20
+print(a + b)
+print("Remainder is",a%b)
+c= input("Enter the value of c:")
+print(type(c))
+
+d= int(input("Enter the no1:"))
+e=int(input("Enter the no2:"))
+print("a is greater than b",a>b)
+print("The avg of this 2 no is",(a+b)/2)  #ang of the number
+print("The square of the no is",a**2)  #square of the number
