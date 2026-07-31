c7ff0465912d1eda3c67c8de34f55adc189bcc14
Gaurav <thombaldnyaneshwari40@gmail.com>
2026-08-01 02:13:25 +0530

    Practice Questions

---
+++ Python/Day14/practice4.py
@@ -0,0 +1,21 @@
+d1= {}
+
+name = input("Enter friends name:")
+lang=input("Enter lanaguage name:")
+d1.update({name: lang})
+
+name = input("Enter friends name:")
+lang=input("Enter lanaguage name:")
+d1.update({name: lang})
+
+name = input("Enter friends name:")
+lang=input("Enter lanaguage name:")
+d1.update({name: lang})
+
+name = input("Enter friends name:")
+lang=input("Enter lanaguage name:")
+d1.update({name: lang})
+
+print(d1)
+
+#if the lang are same then the last lang is print as output
