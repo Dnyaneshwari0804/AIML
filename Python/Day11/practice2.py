0acd8e6fc99b45ea277e61e4925cdbb589368829
Gaurav <thombaldnyaneshwari40@gmail.com>
2026-07-31 23:21:40 +0530

    Completed practice set 2

---
+++ Python/Day11/practice2.py
@@ -0,0 +1,12 @@
+letter = ''' Dear <|Name|>,
+You are selected !
+<|Date|>'''
+print(letter.replace("<|Name|>","Dnyaneshwari").replace("<|Date|>","24 October 2028"))
+
+name ="Dnyanu is a  good girl"
+print(name.find("  "))
+print(name.replace("  "," "))
+print(name) #strings are immutable
+
+letter1 ="Dear Dnyaneshwari,\n\t This python is nice.\nThanks!"
+print(letter1)
