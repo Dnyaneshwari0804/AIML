6cd4e784c4e9f82f2372490759bf9633c7a06305
Gaurav <thombaldnyaneshwari40@gmail.com>
2026-08-04 01:14:09 +0530

    Completed Day15 practice2

--- Python/Day15/practice2.py
+++ Python/Day15/practice2.py
@@ -1,13 +1,13 @@
-m1 = int(input("Enter marks 1"))
-m2 = int(input("Enter marks 2"))
-m3 = int(input("Enter marks 3"))
+m1 = int(input("Enter marks 1:"))
+m2 = int(input("Enter marks 2:"))
+m3 = int(input("Enter marks 3:"))
 
 #check for total percentage
 
-t_per = (m1+ m2+ m3)/300
+t_per = (100*(m1+ m2+ m3))/300
 
-if(t_per>=40):
-    print("You are pass")
+if(t_per>=40 and m1>=33 and m2>=33 and m3>=33):
+    print("You are pass",t_per)
 
 else:
-    print("You, failed tryy next yearrr!!!!!!!!")
+    print("You, failed tryy next yearrr!!!!!!!!",t_per)
