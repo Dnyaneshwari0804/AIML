9e4f40c4b4159a1493914c2988eef0566cef6308
Gaurav <thombaldnyaneshwari40@gmail.com>
2026-08-04 01:24:04 +0530

    Completed Day15 practice4

---
+++ Python/Day15/practice4.py
@@ -0,0 +1,5 @@
+uname = input("Enter username")
+if(len(uname)<10):
+    print("your user name contains less than 10 characters")
+else:
+    print("your user name contains more than 10 characters")
